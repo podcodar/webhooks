c64e6eb614dab2d16e37eafb8d3fcb33de5a9511
Marco <ma.souza.junior@gmail.com>
2021-07-12 16:55:31 -0300

    fix: update project description

--- src/webapp/App.tsx
+++ src/webapp/App.tsx
@@ -3,7 +3,7 @@ import React, { h } from "https://x.lcas.dev/preact@10.5.12/mod.js";
 function App() {
   return (
     <p>
-      Welcome to PodCodar WebHooks! Check our repository <a href="">here</a>
+      Welcome to PodCodar WebHooks! Check our repository <a href="https://github.com/podcodar/webhooks">here</a>
     </p>
   )
 }
