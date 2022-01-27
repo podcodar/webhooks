d1024b8345fac8a1dd6b7dc4e9b38c2fdd4bd198
Marco <ma.souza.junior@gmail.com>
2022-01-27 15:55:30 -0300

    enh: improve daily callback handler

--- src/webhooks/dailyCallbackHandler.ts
+++ src/webhooks/dailyCallbackHandler.ts
@@ -1,6 +1,9 @@
+import { Context } from "../../deps.ts";
 import { DAILY_CHANNEL, DAILY_MESSAGE, sendMessage } from "../services/discord.ts";
 
-export function dailyCallbackHandler() {
-  // TODO: add request validation
+export function dailyCallbackHandler(ctx: Context) {
   sendMessage(DAILY_MESSAGE, DAILY_CHANNEL);
+
+  ctx.response.headers.set("Content-Type", "application/json");
+  ctx.response.body = { message: "Daily Alert sent!" };
 }
